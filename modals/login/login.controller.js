8ee8156dc7b0d24508583957dc6fbd1396afbdb1
Ernesto Laval <ernesto.laval@gmail.com>
2016-07-17 09:40:31 +0100

    Add password reset & emails

--- modals/login/login.controller.js
+++ modals/login/login.controller.js
@@ -7,15 +7,26 @@
  * @ngdoc controller
  *
  */
-angular.module('tideApp').controller('LoginController', ['$rootScope', '$uibModalInstance','authService', LoginController]);
+angular.module('tideApp').controller('LoginController', ['$rootScope', '$uibModalInstance','$translate', 'authService', LoginController]);
 
-function LoginController($rootScope, $uibModalInstance, authService) {
+function LoginController($rootScope, $uibModalInstance,$translate, authService) {
   var myself = this;
-  this.newRegistration = false;
+  myself.registrationMode = registrationMode;
+  myself.forgotPasswordMode = forgotPasswordMode;
 
+
+  this.newRegistration = false;
+  this.message= null;
+  
   /**
    * SignIn
-   */
+   * Error codes:
+   * ERROR_SIGNIN_INVALID_EMAIL
+   * ERROR_SIGNIN_INVALID_PASSWORD
+   * ERROR_SIGNIN_SAVE_TOKEN
+   * ERROR_SIGNIN_NO_EMAIL_PASSWORD
+   *    
+   **/
   this.signIn = function () {
     myself.message = null;
     authService.signIn(myself.email, myself.password)
@@ -24,28 +35,89 @@ function LoginController($rootScope, $uibModalInstance, authService) {
       $uibModalInstance.close({registration: false, email:myself.email, password:myself.password});
     })
     .catch(function(err) {
-      myself.message = "Error in email/password combination";
+      $translate(err ? err : "ERROR_SIGNIN")
+      .then(function(msg) {
+        myself.message = msg;
+      })
+      .catch(function(err) {
+        myself.message = "Error";
+      })
+      
     })
   };
 
   /**
    * SignUp
+   * Error codes:
+   * 
+   * ERROR_SIGNUP
+   * ERROR_SIGNUP_EMAIL_EXISTS
+   * ERROR_SIGNUP_USERNAME_EXISTS
+   * ERROR_SIGNUP_USER_VALIDATION_FAILED
+   * ERROR_SIGNUP_USER_SAVE_ERROR
+   * ERROR_SIGNUP_LACK_EMAIL_USERNAME_PASSWORD
+   * ERROR_SIGNUP_FAILED_ACTIVATION_EMAIL
    */
   this.signUp = function () {
     myself.message = null;
-    authService.signUp(myself.email, myself.password, myself.username)
+    
+    var language= $translate.use() ? $translate.use() : 'en';
+    authService.signUp(myself.email, myself.password, myself.username, language)
     .then(function() {
       myself.message = "Sign Up succesfull";
       myself.signIn();
       myself.newRegistration = false;
     })
     .catch(function(err) {
-      myself.message = "Error in Sign Up :"+err;
+      $translate(err ? err : "ERROR_SIGNUP")
+      .then(function(msg) {
+        myself.message = msg;
+      })
+      .catch(function(err) {
+        myself.message = "Error";
+      })
     })
 
 
     //$uibModalInstance.close({registration: true, email:myself.email, password:myself.password, username:myself.username});
   };
+
+  /**
+   * ResetPassword
+   */
+  this.resetPassword = function () {
+    myself.emailSent = false;
+    myself.emailError = false;
+    myself.message = null;
+    var language= $translate.use() ? $translate.use() : 'en';
+    authService.forgotPassword(myself.email, language)
+    .then(function() {
+      //myself.message = "Email sent to "+myself.email+" to change password";
+      myself.forgotPassword = false;
+      myself.emailSent = true;
+    })
+    .catch(function(err) {
+
+      $translate(err ? err : "ERROR_FORGOT_PASSWORD_REQUEST")
+      .then(function(msg) {
+        myself.message = msg;
+      })
+      .catch(function(err) {
+        myself.message = "Error";
+      })
+    })
+  };
+
+  function registrationMode() {
+    myself.forgotPassword=false;
+    myself.message=false;
+  }
+
+
+  function forgotPasswordMode() {
+    myself.newRegistration=false;
+    myself.message=false;
+  }
 
   /**
    * Cancel
--- services/auth.service.js
+++ services/auth.service.js
@@ -17,11 +17,18 @@ function($rootScope, $http, $q, d3, localstorage, jwtHelper) {
   this.signIn = signIn;
   this.signUp = signUp;
   this.signOut = signOut;
+  this.forgotPassword = forgotPassword;
   this.signInWithToken = signInWithToken;
   this.getAccessToken = getAccessToken;
 
   /**
    * signIn
+   * Error codes
+   * ERROR_SIGNIN
+   * ERROR_SIGNIN_INVALID_EMAIL
+   * ERROR_SIGNIN_INVALID_PASSWORD
+   * ERROR_SIGNIN_SAVE_TOKEN
+   * ERROR_SIGNIN_NO_EMAIL_PASSWORD
    */
   function signIn(email, password) {
     var deferred = $q.defer();
@@ -32,21 +39,20 @@ function($rootScope, $http, $q, d3, localstorage, jwtHelper) {
     },{
       skipAuthorization: true // Prevents from automatically inserting an access_token (which is not available yet)
     })
-    .then(function(res,err) {
+    .then(function(res) {
       var data = res.data;
       if (data.success) {
         localstorage.set("username", data.username);
         localstorage.set("access_token", data.access_token);
         localstorage.set("refresh_token", data.refresh_token);
 
-        //$rootScope.$broadcast("signIn", data.username);
-
         deferred.resolve(data.username);
       } else {
-        deferred.reject("Unsuccesful login");
+
+        deferred.reject(data.message_code ? data.message_code : "ERROR_SIGNIN");
       }
     }, function(res) {
-      deferred.reject("Error conecting to auth service");
+      deferred.reject("ERROR_SIGNIN");
     });
 
     return deferred.promise;
@@ -55,11 +61,20 @@ function($rootScope, $http, $q, d3, localstorage, jwtHelper) {
 
   /**
    * signUp
+   * Error codes:
+   * ERROR_SIGNUP
+   * ERROR_SIGNUP_EMAIL_EXISTS
+   * ERROR_SIGNUP_USERNAME_EXISTS
+   * ERROR_SIGNUP_USER_VALIDATION_FAILED
+   * ERROR_SIGNUP_USER_SAVE_ERROR
+   * ERROR_SIGNUP_LACK_EMAIL_USERNAME_PASSWORD
+   * ERROR_SIGNUP_FAILED_ACTIVATION_EMAIL
    */
-  function signUp(email, password, username) {
+  function signUp(email, password, username, language) {
     var deferred = $q.defer();
 
-    $http.post("https://api.firstmakers.com/api/auth/signup",{
+    $http.post("https://api.firstmakers.com/api/auth/signup?lang="+language",{
+    //$http.post("http://localhost.firstmakers.com:8080/api/auth/signup",{
       'email':email,
       'password':password,
       'username':username
@@ -71,15 +86,44 @@ function($rootScope, $http, $q, d3, localstorage, jwtHelper) {
       if (data.success) {
         deferred.resolve(data.username);
       } else {
-        deferred.reject(data.message);
+        deferred.reject(data.message_code ? data.message_code : "ERROR_SIGNUP");
       }
     }, function(res) {
-      deferred.reject("Error conecting to auth service");
+      deferred.reject("ERROR_SIGNUP");
     });
 
     return deferred.promise;
   }
 
+ /**
+   * forgotPassword
+   * error message_codes:
+   * ERROR_FORGOT_PASSWORD_REQUEST
+   * ERROR_FORGOT_PASSWORD_CHECKING
+   * ERROR_FORGOT_PASSWORD_NON_USER
+   * ERROR_FORGOT_PASSWORD_NO_EMAIL
+   */
+  function forgotPassword(email, language) {
+    var deferred = $q.defer();
+
+    $http.post("https://api.firstmakers.com/api/auth/forgotpassword?lang="+language,{
+      'email':email
+    },{
+      skipAuthorization: true // Prevents from automatically inserting an access_token (which is not available yet)
+    })
+    .then(function(res,err) {
+      var data = res.data;
+      if (data.success) {
+        deferred.resolve(data);
+      } else {
+        deferred.reject(data.message_code ? data.message_code : "ERROR_FORGOT_PASSWORD_REQUEST");
+      }
+    }, function(res) {
+      deferred.reject("ERROR_FORGOT_PASSWORD_REQUEST");
+    });
+
+    return deferred.promise;
+  }
 
   /**
    * signInWithToken
